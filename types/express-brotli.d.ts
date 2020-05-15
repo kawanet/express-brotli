e6155c9f850784e8fe9aeb44cf3dcf97e0b6ecc3
kawanet <u-suke@kawa.net>
2020-05-15 16:40:40 +0900

    tsc

--- types/express-brotli.d.ts
+++ types/express-brotli.d.ts
@@ -1,13 +1,22 @@
 import { RequestHandler } from "express";
+declare type Tester = {
+    test: (str: string) => boolean;
+};
+export interface CompressOptions {
+    contentLength?: RegExp | Tester;
+    contentType?: RegExp | Tester;
+    statusCode?: RegExp | Tester;
+}
 /**
  * Returns an RequestHandler to compress the Express.js response stream.
  * It performs compression only for text-ish `Content-Type`s which includes `/^text|json|javascript|svg|xml|utf-8/i` per default.
- * It supports `Accept-Encoding` request header and `Content-Encoding` response headers.
+ * It supports `Accept-Encoding` request header and `Content-Encoding` response header.
  */
-export declare function compress(contentType?: RegExp): RequestHandler;
+export declare function compress(options?: CompressOptions): RequestHandler;
 /**
  * Returns an RequestHandler to decompress the Express.js response stream.
  * It performs decompression for any `Content-Type`s if `contentType` parameter is not specified.
- * It supports `Accept-Encoding` request header and `Content-Encoding` response headers.
+ * It supports `Accept-Encoding` request header and `Content-Encoding` response header.
  */
-export declare function decompress(contentType?: RegExp): RequestHandler;
+export declare function decompress(options?: CompressOptions): RequestHandler;
+export {};
